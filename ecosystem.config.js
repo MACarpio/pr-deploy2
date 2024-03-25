acec7816ea1833a9be55f7715b03ce7b0ba6daad
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2024-03-25 04:06:38 -0500

    add path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       host : '44.211.56.122',
       ref  : 'origin/main',
       repo : 'git@github.com:MACarpio/pr-deploy2.git',
-      path : 'DESTINATION_PATH',
+      path : '/home/bitnami',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
